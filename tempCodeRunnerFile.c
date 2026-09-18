4bcf4aacaacd0955cdb3437cf07bad9002c53b11
Zack <artokalo04@gmail.com>
2026-09-18 23:25:56 +0200

    Premier commit – Création du projet HelloGit

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main() {
+    int moyO; 
+    int N[];
+    for  int i in range(0,6){
+        printf("entrer votre note : \n ", i++ );
+        scanf("%s", &N[i]);
+        int somme += N[i];
+       int  min=N[i];
+        int max = N[i];
+        moyO = somme - min - max / 7; 
+
+        while (somme > -1 || somme < 20){
+            printf("la note la plus basse est : %d \n", min, " ont été retirées " , "la  moyenne est : %d \n", moy 0, ,"la note la plus haute est : %d \n", max);
+            printf("la moyenne olympique est : %d", moyO)
+        }
+    }
+}
